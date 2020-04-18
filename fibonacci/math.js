8611cc63cdc4817a7e2cdb492c1644225c93af84
nguyen.nhu.dat@sun-asterisk.com <nnd.nguyennhudat@gmail.com>
2020-04-18 16:47:22 +0700

    chapter 4-fibonacci not optimize

---
+++ fibonacci/math.js
@@ -0,0 +1,4 @@
+var fibonacci = exports.fibonacci = function(n) {
+  if(n == 0 || n == 1) return 1;
+  else return fibonacci(n - 1) + fibonacci(n - 2);
+}
---
+++ fibonacci/routes/fibonacci.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var math = require('../math');
+router.get('/', function(req, res, next) {
+  if(req.query.fibonum) {
+    res.render('fibonacci', {
+      title: "Calculate fibonacci number",
+      fibonum: req.query.fibonum,
+      fiboval: math.fibonacci(req.query.fibonum)
+    });
+  } else {
+    res.render('fibonacci', {
+      title: "Calculate fibonacci number",
+      fiboval: undefined
+    })
+  }
+});
+
+module.exports = router;
