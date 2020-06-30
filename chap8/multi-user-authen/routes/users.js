3f873de4a26a49eb80a6a6e92024e9090a335681
datnguyen199 <nnd.nguyennhudat@gmail.com>
2020-06-30 23:39:07 +0700

    Login, logout with passport

--- chap8/multi-user-authen/routes/users.js
+++ chap8/multi-user-authen/routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const express = require('express');
+const path = require('path');
+const log = require('debug')('notes:router-users');
+const error = require('debug')('notes-error');
+const router = express.Router();
+
+exports.router = router;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const usersModel = require(process.env.USERS_MODEL ? path.join('..', process.env.USERS_MODEL) : '../models/users-rest');
+
+exports.initPassport = function(app) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+}
+
+exports.ensureAuthenticated = function(req, res, next) {
+  if(req.user) next();
+  else res.redirect('users/login');
+}
 
 module.exports = router;
